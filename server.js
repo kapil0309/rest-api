0c3008f89ef15ba4951cc05b16fbfd57d42a431a
kapil0309 <kapil.yadav3@tcs.com>
2020-08-25 12:58:58 +0530

    changes in docker file

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ db.mongoose
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to student-data application." });
 });
 
 require("./app/routes/student.routes")(app);
